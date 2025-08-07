08fe55cc763a231791c22624f84e4f87845c3888
Akshay Nile <akshaynile8849@gmail.com>
2025-08-07 16:43:33 +0530

    added api for exploring and searching items

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,38 @@
-import flask
+from flask import Flask, jsonify, request
+from werkzeug.exceptions import HTTPException
 
-app = flask.Flask(__name__)
+from services.decorators import validate_path
+from services.explorer import get_drives_info, get_items_info
+
+app = Flask(__name__)
 
 
+# To serve the UI app build (under development)
 @app.route('/', methods=['GET'])
 def home():
-    return flask.jsonify({'message': 'Hello from Python-Flask!'})
+    return jsonify({'message': 'Hello from Python-Flask!'})
+
+
+# To get info about drives or items at the given path
+@app.route('/api/items', methods=['GET'])
+@validate_path
+def get_items(path):
+    if path == '/':
+        return jsonify(get_drives_info())
+    search = request.args.get('search', None)
+    folders, files = get_items_info(path, search=search)
+    return jsonify({'folders': folders, 'files': files})
+
+
+# Global http error handler to get jsonified error response
+@app.errorhandler(HTTPException)
+def handle_http_exception(error):
+    response = {
+        "error": error.name,
+        "message": error.description,
+        "code": error.code
+    }
+    return jsonify(response), error.code
 
 
 if __name__ == '__main__':
---
+++ backend/services/explorer.py
@@ -0,0 +1,119 @@
+import os
+import ctypes
+from shutil import disk_usage
+from stat import FILE_ATTRIBUTE_HIDDEN
+
+
+def format_size(b: int):
+    if b > 1024*1024*1024:
+        return f'{round(b/1024/1024/1024, 2)} GB'
+    elif b > 1024*1024:
+        return f'{round(b/1024/1024, 1)} MB'
+    elif b > 1024:
+        return f'{round(b/1024)} KB'
+    else:
+        return f'{b} B'
+
+
+def get_drive_label(drive_letter: str):
+    drive_label_buffer = ctypes.create_unicode_buffer(1024)
+
+    result = ctypes.windll.kernel32.GetVolumeInformationW(
+        ctypes.c_wchar_p(drive_letter),
+        drive_label_buffer,
+        ctypes.sizeof(drive_label_buffer), None, None, None, None, 0)
+
+    if result:
+        return drive_label_buffer.value or f"Drive {drive_letter[:2]}"
+    return "Unknown"
+
+
+def is_item_hidden(item_path, item_name):
+    try:
+        marked_hidden = item_name[0] in '.$'
+        actual_hidden = bool(os.stat(item_path).st_file_attributes & FILE_ATTRIBUTE_HIDDEN)
+        return marked_hidden or actual_hidden
+    except Exception:
+        return True
+
+
+def deep_search(query, root):
+    for path, folders, files in os.walk(root):
+        for item in folders + files:
+            if query in item.lower():
+                yield f'{path.replace("\\", "/")}/{item}'
+
+
+# To get info about storage/disk drives/partitions
+def get_drives_info():
+    drives_info = []
+
+    for letter in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        path = f'{letter}:'
+        if not os.path.exists(path):
+            continue
+        label = get_drive_label(path)
+        total, used, free = map(format_size, disk_usage(path))
+        drives_info.append({
+            'letter': letter, 'label': label, 'path': path,
+            'total': total, 'used': used, 'free': free
+        })
+
+    return drives_info
+
+
+# To get info about folder present at path
+def get_items_info(path: str, sort_by='size', reverse=False, show_hidden=False, search=None):
+    files, folders = [], []
+
+    if not search:
+        items = map(lambda i: f'{path}/{i}', os.listdir(path))
+    else:
+        items = deep_search(search.lower().strip(), path)
+
+    # Only filtered items will be considered if search query is provided
+    for item_path in items:
+        item_name = item_path.split('/')[-1]
+
+        # Hidden items will be skipped if unless show_hidden is true
+        is_hidden = is_item_hidden(item_path, item_name)
+        if is_hidden and not show_hidden:
+            continue
+
+        item_info: dict = {'is_hidden': is_hidden}
+
+        # Setting item_info depending on wheather it's a file or a folder
+        try:
+            if os.path.isdir(item_path):
+                item_info['folder_name'] = item_name
+                item_info['folder_path'] = item_path
+                sub_items = tuple(map(lambda i: f'{item_path}/{i}', os.listdir(item_path)))
+                item_info['folder_count'] = len(tuple(filter(os.path.isdir, sub_items)))
+                item_info['file_count'] = len(tuple(filter(os.path.isfile, sub_items)))
+                item_info['creation_date'] = int(os.path.getctime(item_path))
+                folders.append(item_info)
+            elif os.path.isfile(item_path):
+                item_info['file_name'] = item_name
+                item_info['file_path'] = item_path
+                item_info['file_size'] = format_size(os.path.getsize(item_path))
+                item_info['modification_date'] = int(os.path.getmtime(item_path))
+                files.append(item_info)
+        except PermissionError:
+            print('Access Denied:', item_path)
+            continue
+
+    # Sorting folders and files saperately depending on the given sort_by key
+    if sort_by == 'name':
+        folders.sort(key=lambda x: x['folder_name'], reverse=reverse)
+        files.sort(key=lambda x: x['file_name'], reverse=reverse)
+    elif sort_by == 'type':
+        folders.sort(key=lambda x: x['folder_name'], reverse=reverse)
+        files.sort(key=lambda x: x['file_name'].split('.')[-1], reverse=reverse)
+    elif sort_by == 'date':
+        folders.sort(key=lambda x: x['creation_date'], reverse=reverse)
+        files.sort(key=lambda x: x['modification_date'], reverse=reverse)
+    elif sort_by == 'size':
+        folders.sort(key=lambda x: x['folder_count'] + x['file_count'], reverse=reverse)
+        files.sort(key=lambda x: os.path.getsize(x['file_path']), reverse=reverse)
+
+    return folders, files
