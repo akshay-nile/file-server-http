fa98b164d3980533427d7fae3df38e14d607386f
Akshay Nile <akshaynile8849@gmail.com>
2025-10-27 00:09:52 +0530

    added png images for server-offline and empty-folder

--- frontend/src/components/EmptyFolder.tsx
+++ frontend/src/components/EmptyFolder.tsx
@@ -1,7 +1,8 @@
 function EmptyFolder() {
     return (
-        <div className='h-[66%] flex justify-center items-center text-2xl text-gray-700'>
-            Folder Empty
+        <div className='h-[66vh] flex flex-col justify-center items-center text-center text-2xl text-gray-600'>
+            <img src='/public/icons/empty-folder.png' width='180px' />
+            <div className='my-8 font-semibold'>Empty Folder</div>
         </div>
     );
 }
--- frontend/src/components/ServerOffline.tsx
+++ frontend/src/components/ServerOffline.tsx
@@ -1,7 +1,7 @@
 function ServerOffline() {
     return (
         <div className='h-[90vh] flex flex-col justify-center items-center text-center text-2xl text-gray-100'>
-            <i className='pi pi-globe' style={{ fontSize: '14rem' }} />
+            <img src='/public/icons/server-offline.png' width='200px' />
             <div className='my-8 font-semibold tracking-wide'>Server is Unreachable</div>
             <span className='text-xs font-extralight leading-3.5'>
                 Refresh this page manually <br />
