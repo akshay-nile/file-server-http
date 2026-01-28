72695800ff745a6ab4b85d3bf8090d5c289ec0da
Akshay Nile <akshaynile8849@gmail.com>
2026-01-28 18:13:55 +0530

    added dist to git repo to make installer independent of Node/npm

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -12,31 +12,31 @@ Set-Location ".."
 if (Test-Path "MyFileServer") {
     Remove-Item "MyFileServer" -Recurse -Force
 }
-New-Item -ItemType Directory -Path "MyFileServer/services"
-New-Item -ItemType File -Path "MyFileServer/.nomedia"
+New-Item -ItemType Directory -Path "MyFileServer\services"
+New-Item -ItemType File -Path "MyFileServer\.nomedia"
 
 
 # Step 2) -------- Build frontend and move the generated dist as public --------
 
 Write-Host "`nStep 2) Building the frontend"
 Set-Location "frontend"
-if (-not (Test-Path "node_modules")) {
+if (Get-Command npm -ErrorAction SilentlyContinue) {
     npm ci
-}
-npm run build
+    npm run build
+} 
 if (-not (Test-Path "dist")) {
-    Write-Host "`nFrontend Build Failed" -ForegroundColor Red
+    Write-Host "`nFrontend build not found" -ForegroundColor Red
     exit 1
 }
-Move-Item -Path "dist" -Destination "..\MyFileServer\public"
+Copy-Item -Path "dist" -Destination "..\MyFileServer\public" -Recurse -Force
 
 
 # Step 3) -------- Copy required python backend files to MyFileServer --------
 
 Write-Host "`nStep 3) Copying the backend files"
-Set-Location "../backend"
+Set-Location "..\backend"
 Copy-Item -Path "server.py" -Destination "..\MyFileServer"
-Copy-Item -Path "services/*.py" -Destination "..\MyFileServer\services"
+Copy-Item -Path "services\*.py" -Destination "..\MyFileServer\services"
 
 
 # Step 4) -------- Verifying the final MyFileServer folder structure --------
