3ad242a3054f8ba5e0a8d899137ac77b15ea4fab
Akshay Nile <akshaynile8849@gmail.com>
2026-06-27 21:29:14 +0530

    added beat detection mode

--- frontend/src/components/MusicVisualizer.tsx
+++ frontend/src/components/MusicVisualizer.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useToastMessage } from '../contexts/ToastMessage/useToastMessage';
 
 type Props = { audioNode: AudioNode };
 
 function MusicVisualizer({ audioNode }: Props) {
+    const { showToast } = useToastMessage();
+    const fistStartRef = useRef<boolean>(true);
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [beatMode, setBeatMode] = useState<boolean>(false);
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -13,8 +17,6 @@ function MusicVisualizer({ audioNode }: Props) {
         const context = canvas.getContext('2d');
 
         const splitter = audioNode.context.createChannelSplitter(2);
-        audioNode.connect(splitter);
-        audioNode.connect(audioNode.context.destination);
 
         const leftAnalyser = audioNode.context.createAnalyser();
         leftAnalyser.fftSize = 512;
@@ -24,8 +26,26 @@ function MusicVisualizer({ audioNode }: Props) {
         rightAnalyser.fftSize = 512;
         rightAnalyser.smoothingTimeConstant = 0;
 
+        audioNode.connect(audioNode.context.destination);
+
+        if (beatMode) {
+            const filter = audioNode.context.createBiquadFilter();
+            filter.type = 'lowpass';
+            filter.frequency.value = 100;
+            filter.Q.value = 2;
+            audioNode.connect(filter);
+            filter.connect(splitter);
+        } else audioNode.connect(splitter);
+
         splitter.connect(leftAnalyser, 0);
         splitter.connect(rightAnalyser, 1);
+
+        if (!fistStartRef.current) showToast({
+            severity: 'info',
+            summary: 'Low Pass Filter ' + (beatMode ? 'ON' : 'OFF'),
+            detail: 'Visualizer shows ' + (beatMode ? '(bass) beats only.' : 'sound intensity.')
+        });
+        fistStartRef.current = false;
 
         const leftData = new Float32Array(leftAnalyser.frequencyBinCount);
         const rightData = new Float32Array(rightAnalyser.frequencyBinCount);
@@ -36,7 +56,7 @@ function MusicVisualizer({ audioNode }: Props) {
         let smoothLeft = 0;
         let smoothRight = 0;
         const jitter = 0.4;
-        const rmsGain = 2.2;
+        const rmsGain = 2;
 
         const LEDCount = 26;
         const LEDGap = 2;
@@ -121,11 +141,15 @@ function MusicVisualizer({ audioNode }: Props) {
             cancelAnimationFrame(animationId);
             leftAnalyser.disconnect();
             rightAnalyser.disconnect();
+            splitter.disconnect();
             audioNode.disconnect();
         };
-    }, [audioNode]);
+    }, [audioNode, beatMode, showToast]);
 
-    return <canvas ref={canvasRef} className="w-full h-[35px]" />;
+    return <canvas
+        ref={canvasRef}
+        className="w-full h-[35px]"
+        onClick={() => setBeatMode(prev => !prev)} />;
 }
 
 export default MusicVisualizer;
